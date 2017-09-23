208e3a0874b57919ac43eaec2116c8416a5c23dc
Alan M. Carroll <amc@apache.org>
2017-09-23 11:49:45 -0500

    TsLuaConfig: Static schema data initialization.

--- iocore/net/LuaSNIConfig.h
+++ iocore/net/LuaSNIConfig.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   LuaSNIConfig.h
  * Author: persia
  *
@@ -15,41 +15,38 @@
 #define LUASNICONFIG_H
 
 #include "tsconfig/TsConfigLua.h"
-#include "tsconfig/Errata.h"
-using namespace ts;
-class Errata;
+
+#include <vector>
+
+using ts::Errata;
+
 struct LuaSNIConfig : public TsConfigBase {
-   using self = LuaSNIConfig;
-   enum class Action { CLOSE, TUNNEL };
+  using self = LuaSNIConfig;
+  enum class Action { CLOSE, TUNNEL };
 
-   static TsConfigArrayDescriptor DESCRIPTOR;
-   LuaSNIConfig() : TsConfigBase(DESCRIPTOR) {}
+  static TsConfigArrayDescriptor DESCRIPTOR;
 
-   struct Item : public TsConfigBase {
-      Item() : TsConfigBase(DESCRIPTOR)
-         {
-          Item::FQDN_CONFIG = TsConfigString<Item>(FQDN_DESCRIPTOR, fqdn);
-         // Item::LEVEL_CONFIG = TsConfigInt<Item>(LEVEL_DESCRIPTOR, self::Item::level);
-          Item::ACTION_CONFIG = TsConfigEnum<Item,self::Action>(ACTION_DESCRIPTOR,action);
-      }
-      ts::Errata loader(lua_State* s) override;
+  LuaSNIConfig() : TsConfigBase(DESCRIPTOR) {}
 
-      std::string fqdn;
-      int level;
-      Action action;
+  struct Item : public TsConfigBase {
+    Item() : TsConfigBase(DESCRIPTOR), FQDN_CONFIG(FQDN_DESCRIPTOR, fqdn), ACTION_CONFIG(ACTION_DESCRIPTOR, action)  {}
+    ts::Errata loader(lua_State* s) override;
 
-      // These need to be initialized statically.
-      static TsConfigObjectDescriptor OBJ_DESCRIPTOR;
-      static TsConfigDescriptor FQDN_DESCRIPTOR;
-      static TsConfigString<Item> FQDN_CONFIG;
-      //static TsConfigDescriptor LEVEL_DESCRIPTOR;
-      //static TsConfigInt<Item> LEVEL_CONFIG;
-      static TsConfigEnumDescriptor ACTION_DESCRIPTOR;
-      static TsConfigEnum<Item, Action> ACTION_CONFIG;
-   };
-   static std::vector<Item> items;
-   ts::Errata loader(lua_State* s) override;
+    std::string fqdn;
+    int level;
+    Action action;
+
+    // These need to be initialized statically.
+    static TsConfigObjectDescriptor OBJ_DESCRIPTOR;
+    static TsConfigDescriptor FQDN_DESCRIPTOR;
+    TsConfigString FQDN_CONFIG;
+    //static TsConfigDescriptor LEVEL_DESCRIPTOR;
+    //static TsConfigInt<Item> LEVEL_CONFIG;
+    static TsConfigEnumDescriptor ACTION_DESCRIPTOR;
+    TsConfigEnum<Action> ACTION_CONFIG;
+  };
+  std::vector<Item> items;
+  ts::Errata loader(lua_State* s) override;
 };
 
 #endif /* LUASNICONFIG_H */
-
